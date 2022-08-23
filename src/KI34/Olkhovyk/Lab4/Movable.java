611a351aaa61cf7209fd791bcd74030f04bdb826
Alex Olkhovyk <98950545+SlenderSS@users.noreply.github.com>
2022-08-23 15:44:13 +0300

    First publish
    labs: 1, 2, 3, 4, 5, 6.

---
+++ src/KI34/Olkhovyk/Lab4/Movable.java
@@ -0,0 +1,5 @@
+package KI34.Olkhovyk.Lab4;
+
+public interface Movable {
+	void Move(int cycles);
+}
---
+++ src/KI34/Olkhovyk/Lab5/CalcException.java
@@ -0,0 +1,10 @@
+package KI34.Olkhovyk.Lab5;
+
+public class CalcException extends ArithmeticException {
+    public CalcException() {
+    }
+
+    public CalcException(String cause) {
+        super(cause);
+    }
+}
